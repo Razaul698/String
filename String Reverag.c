1603961095f62c021edda20a6ab1c6aef838b94e
MD. RAZAUL KARIM <mdrazaulkarim698@gmail.com>
2025-12-07 18:38:20 +0600

    Add files via upload

---
+++ String Reverag.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<string.h>
+int main()
+{
+   char str[]="Razaul karim";
+   printf("Original = %s\n",str);
+
+   strrev(str);
+
+   printf("Original = %s\n",str);
+
+
+    return 0;
+}
+
