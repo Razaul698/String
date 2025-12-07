a1cefca4660ab4bffff0b1b921287b7ee8d3661d
MD. RAZAUL KARIM <mdrazaulkarim698@gmail.com>
2025-12-07 10:37:30 +0600

    Add files via upload

---
+++ String Concanate.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<string.h>
+int main()
+{
+    char str1[]="My name is ";
+    //char str2[]="Razaul Karim";
+
+    strcat(str1,"Razaul karim");
+
+    printf("Full Name = %s\n",str1);
+
+
+
+}
